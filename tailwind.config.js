44e54a38360925421b5d7fb03ed665131dbbdbb7
talhatonmoy <talha.tonmoy.ck@gmail.com>
2023-07-26 13:01:53 +0600

    tailwind form

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#AF1D2C",
+        offwhite: "#E0E0E0",
+      },
+    },
+  },
+  plugins: [],
+};
+
